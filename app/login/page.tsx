98efe1c596c93bf2271716ee325dacc7225c0db3
frank-job <jobf0837@gmail.com>
2026-08-25 12:49:07 +0300

    clean ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,12 +6,16 @@ export default function LoginPage() {
 <main className="flex items-center justify-center min-h-screen bg-surface px-4 py-12">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-6">
         {/* Header / Logo */}
-         <div className="flex items-center justify-center">
-                   <RatLogo size="lg" />
-                </div>
+         {/* <div className="flex items-center justify-center"> */}
+                   {/* <RatLogo size="lg" /> */}
+                {/* </div> */}
         {/* <div className="flex h-32 w-full items-center justify-center rounded-3xl p-6 shadow-lg" /> */}
 
         {/* Login Form Component */}
+         <div className="flex items-center justify-center">
+                   <RatLogo size="lg" />
+                </div>
+               
         <LoginForm />
       </div>
     </main>
