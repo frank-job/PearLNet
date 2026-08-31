02fb9ad16417c5217f99b9a1235bda38e7dc8d50
frank-job <jobf0837@gmail.com>
2026-08-31 14:17:16 +0300

    local

---
+++ app/ui/local.tsx
@@ -0,0 +1,33 @@
+# Recommended for most uses
+DATABASE_URL=postgresql://neondb_owner:npg_tux1y6TRLeGW@ep-misty-bread-aui8eb2z-pooler.c-10.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require
+
+# For uses requiring a connection without pgbouncer
+DATABASE_URL_UNPOOLED=postgresql://neondb_owner:npg_tux1y6TRLeGW@ep-misty-bread-aui8eb2z.c-10.us-east-1.aws.neon.tech/neondb?sslmode=require
+
+# Parameters for constructing your own connection string
+PGHOST=ep-misty-bread-aui8eb2z-pooler.c-10.us-east-1.aws.neon.tech
+PGHOST_UNPOOLED=ep-misty-bread-aui8eb2z.c-10.us-east-1.aws.neon.tech
+PGUSER=neondb_owner
+PGDATABASE=neondb
+PGPASSWORD=npg_tux1y6TRLeGW
+
+# Parameters for Vercel Postgres Templates
+POSTGRES_URL=postgresql://neondb_owner:npg_tux1y6TRLeGW@ep-misty-bread-aui8eb2z-pooler.c-10.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require
+POSTGRES_URL_NON_POOLING=postgresql://neondb_owner:npg_tux1y6TRLeGW@ep-misty-bread-aui8eb2z.c-10.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require
+POSTGRES_USER=neondb_owner
+POSTGRES_HOST=ep-misty-bread-aui8eb2z-pooler.c-10.us-east-1.aws.neon.tech
+POSTGRES_PASSWORD=npg_tux1y6TRLeGW
+POSTGRES_DATABASE=neondb
+POSTGRES_URL_NO_SSL=postgresql://neondb_owner:npg_tux1y6TRLeGW@ep-misty-bread-aui8eb2z-pooler.c-10.us-east-1.aws.neon.tech/neondb
+POSTGRES_PRISMA_URL=postgresql://neondb_owner:npg_tux1y6TRLeGW@ep-misty-bread-aui8eb2z-pooler.c-10.us-east-1.aws.neon.tech/neondb?channel_binding=require&connect_timeout=15&sslmode=require
+
+AUTH_SECRET=Ev7gIHgmX6X8Yj0cMseUOd1xMIvI/u16n3QMnA0BpKU=
+
+AUTH_URL=https://rat-git-main-frankjob.vercel.app/
+NEWS_API_KEY=bdb91951490d4440baeaf2d82f514c60
+
+BLOB_READ_WRITE_TOKEN="vercel_blob_rw_0ak9TeztxiaxlEKv_BiR82veSUwzYz4zVC0n8ulZ9gjjzVI"
+
+
+footballApi=58f01760813c0f0e52728f31f8e43482
+58f01760813c0f0e52728f31f8e43482
